0b6698912ad474d3f8032ffa6c81dbc5b606a0f5
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2021-03-11 15:14:46 +0200

    fix the spaces apearing in the PR

--- src/Event/ControllerListener.php
+++ src/Event/ControllerListener.php
@@ -22,7 +22,7 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
  */
 class ControllerListener
 {
-
+    
     /** @var array|null */
     protected $arrayRouter;
     /** @var GroupServiceInterface */
@@ -33,7 +33,7 @@ class ControllerListener
     protected $session;
     /** @var TokenStorageInterface */
     protected $tokenStorage;
-
+    
     /**
      * ControllerListener constructor.
      *
@@ -50,7 +50,7 @@ class ControllerListener
         $this->session = $session;
         $this->tokenStorage = $tokenStorage;
     }
-
+    
     /**
      * @param FilterControllerEvent $event
      */
@@ -60,7 +60,7 @@ class ControllerListener
         $route = $request->attributes->get('_route');
         $controller = $event->getController();
         $service = $this->groupService;
-
+        
         if ($controller[0] instanceof Controller && !empty($route)) {
             $user = $this->getUser();
             if ($user instanceof AdminUserInterface && $user->getGroup() instanceof Group) {
@@ -75,7 +75,7 @@ class ControllerListener
             }
         }
     }
-
+    
     /**
      * @param string                $route
      * @param string                $message
@@ -88,7 +88,7 @@ class ControllerListener
             return new RedirectResponse($route);
         });
     }
-
+    
     /**
      * @return UserInterface|null
      */
@@ -97,7 +97,7 @@ class ControllerListener
         if (null === $token = $this->tokenStorage->getToken()) {
             return null;
         }
-
+        
         if (!is_object($user = $token->getUser())) {
             // e.g. anonymous authentication
             return null;
@@ -105,4 +105,4 @@ class ControllerListener
 
         return $user;
     }
-}
+} 

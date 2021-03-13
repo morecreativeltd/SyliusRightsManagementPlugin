1969095a87deb9b3cb024fbd28e00dcb81b4fbe8
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2021-03-13 20:56:53 +0200

    GroupController.php fix white spaces

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -30,54 +30,54 @@ class GroupController extends ResourceController
     public function createAction(Request $request): Response
     {
         $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
+        
         $this->isGrantedOr403($configuration, ResourceActions::CREATE);
         /** @var GroupInterface $newResource */
         $newResource = $this->newResourceFactory->create($configuration, $this->factory);
-
+        
         $service = $this->get('be_happy.rights_management.service.group');
         $service->createMissingRights($newResource);
-
+        
         $form = $this->resourceFormFactory->create($configuration, $newResource);
-
+        
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $newResource = $form->getData();
-
+            
             $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::CREATE, $configuration, $newResource);
-
+            
             if ($event->isStopped() && !$configuration->isHtmlRequest()) {
                 throw new HttpException($event->getErrorCode(), $event->getMessage());
             }
             if ($event->isStopped()) {
                 $this->flashHelper->addFlashFromEvent($configuration, $event);
-
+                
                 if ($event->hasResponse()) {
                     return $event->getResponse();
                 }
-
+                
                 return $this->redirectHandler->redirectToIndex($configuration, $newResource);
             }
-
+            
             if ($configuration->hasStateMachine()) {
                 $this->stateMachine->apply($configuration, $newResource);
             }
-
+            
             $this->repository->add($newResource);
             $postEvent = $this->eventDispatcher->dispatchPostEvent(ResourceActions::CREATE, $configuration, $newResource);
-
+            
             if (!$configuration->isHtmlRequest()) {
                 return $this->viewHandler->handle($configuration, View::create($newResource, Response::HTTP_CREATED));
             }
-
+            
             $this->flashHelper->addSuccessFlash($configuration, ResourceActions::CREATE, $newResource);
-
+            
             if ($postEvent->hasResponse()) {
                 return $postEvent->getResponse();
             }
-
+            
             return $this->redirectHandler->redirectToResource($configuration, $newResource);
         }
-
+        
         if (!$configuration->isHtmlRequest()) {
             return $this->viewHandler->handle($configuration, View::create($form, Response::HTTP_BAD_REQUEST));
         }
@@ -96,7 +96,7 @@ class GroupController extends ResourceController
             'form' => $form->createView(),
         ]);
     }
-
+    
     /**
      * @param Request $request
      *
@@ -107,65 +107,64 @@ class GroupController extends ResourceController
     public function updateAction(Request $request): Response
     {
         $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
+        
         $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
         /** @var GroupInterface $resource */
         $resource = $this->findOr404($configuration);
-
+        
         $service = $this->get('be_happy.rights_management.service.group');
         $service->createMissingRights($resource);
-
+        
         $form = $this->resourceFormFactory->create($configuration, $resource);
-
+        
         if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true) && $form->handleRequest($request)->isValid()) {
             $resource = $form->getData();
-
+            
             /** @var ResourceControllerEvent $event */
             $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::UPDATE, $configuration, $resource);
-
+            
             if ($event->isStopped() && !$configuration->isHtmlRequest()) {
                 throw new HttpException($event->getErrorCode(), $event->getMessage());
             }
             if ($event->isStopped()) {
                 $this->flashHelper->addFlashFromEvent($configuration, $event);
-
+                
                 if ($event->hasResponse()) {
                     return $event->getResponse();
                 }
 
                 return $this->redirectHandler->redirectToResource($configuration, $resource);
             }
-
+            
             $this->resourceUpdateHandler->handle($resource, $configuration, $this->manager);
-
-
+            
             $postEvent = $this->eventDispatcher->dispatchPostEvent(ResourceActions::UPDATE, $configuration, $resource);
-
+            
             if (!$configuration->isHtmlRequest()) {
                 $view = $configuration->getParameters()->get('return_content', false) ? View::create($resource, Response::HTTP_OK) : View::create(null, Response::HTTP_NO_CONTENT);
-
+                
                 return $this->viewHandler->handle($configuration, $view);
             }
-
+            
             $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $resource);
-
+            
             if ($postEvent->hasResponse()) {
                 return $postEvent->getResponse();
             }
-
+            
             return $this->redirectHandler->redirectToResource($configuration, $resource);
         }
-
+        
         if (!$configuration->isHtmlRequest()) {
             return $this->viewHandler->handle($configuration, View::create($form, Response::HTTP_BAD_REQUEST));
         }
-
+        
         $initializeEvent = $this->eventDispatcher->dispatchInitializeEvent(ResourceActions::UPDATE, $configuration, $resource);
         $initializeEventResponse = $initializeEvent->getResponse();
         if (null !== $initializeEventResponse) {
             return $initializeEventResponse;
         }
-
+        
         return $this->render($configuration->getTemplate(ResourceActions::UPDATE . '.html'), [
             'configuration' => $configuration,
             'metadata' => $this->metadata,

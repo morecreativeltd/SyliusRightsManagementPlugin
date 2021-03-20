0eb92f039e73d7791ed4be44c262caba1e2ea6c1
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2021-03-20 13:40:47 +0200

    fix the routes

--- src/Event/ControllerListener.php
+++ src/Event/ControllerListener.php
@@ -7,6 +7,7 @@ namespace BeHappy\SyliusRightsManagementPlugin\Event;
 use BeHappy\SyliusRightsManagementPlugin\Entity\AdminUserInterface;
 use BeHappy\SyliusRightsManagementPlugin\Entity\Group;
 use BeHappy\SyliusRightsManagementPlugin\Service\GroupServiceInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Sylius\Component\User\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -63,7 +64,7 @@ class ControllerListener
         $controller = $event->getController();
         $service = $this->groupService;
 
-        if (!($controller instanceof ErrorController) &&$controller[0] instanceof Controller && !empty($route)) {
+        if (!($controller instanceof ErrorController) && $controller[0] instanceof ResourceController && !empty($route)) {
             $user = $this->getUser();
             if ($user instanceof AdminUserInterface && !empty($user->getGroup()) && $user->getGroup() instanceof Group) {
                 if (!$service->isUserGranted($route, $user)) {
